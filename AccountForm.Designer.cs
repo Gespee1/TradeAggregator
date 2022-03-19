881415f9a9e6b5b8ad5a95cb9765568f153d68f2
Денис Белотелов <BelotelovDenis@yandex.ru>
2022-03-19 19:39:26 +0700

    Подключил базу. Доработал форму авторизации. Подключил функционал к форме авторизации. Создал форму личного кабинета.

---
+++ AccountForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace TradeAggregator
+{
+    partial class AccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMain = new System.Windows.Forms.Label();
+            this.buttonProfile = new System.Windows.Forms.Button();
+            this.buttonVendors = new System.Windows.Forms.Button();
+            this.buttonOrder = new System.Windows.Forms.Button();
+            this.buttonKP = new System.Windows.Forms.Button();
+            this.buttonContracts = new System.Windows.Forms.Button();
+            this.buttonGraph = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMain
+            // 
+            this.labelMain.AutoSize = true;
+            this.labelMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMain.Location = new System.Drawing.Point(32, 21);
+            this.labelMain.Name = "labelMain";
+            this.labelMain.Size = new System.Drawing.Size(230, 20);
+            this.labelMain.TabIndex = 1;
+            this.labelMain.Text = "Вы авторизовались как ...";
+            // 
+            // buttonProfile
+            // 
+            this.buttonProfile.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonProfile.Location = new System.Drawing.Point(12, 253);
+            this.buttonProfile.Name = "buttonProfile";
+            this.buttonProfile.Size = new System.Drawing.Size(240, 30);
+            this.buttonProfile.TabIndex = 6;
+            this.buttonProfile.Text = "Профиль";
+            this.buttonProfile.UseVisualStyleBackColor = true;
+            // 
+            // buttonVendors
+            // 
+            this.buttonVendors.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonVendors.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonVendors.Location = new System.Drawing.Point(12, 289);
+            this.buttonVendors.Name = "buttonVendors";
+            this.buttonVendors.Size = new System.Drawing.Size(240, 30);
+            this.buttonVendors.TabIndex = 7;
+            this.buttonVendors.Text = "Поставщики";
+            this.buttonVendors.UseVisualStyleBackColor = true;
+            // 
+            // buttonOrder
+            // 
+            this.buttonOrder.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOrder.Location = new System.Drawing.Point(12, 325);
+            this.buttonOrder.Name = "buttonOrder";
+            this.buttonOrder.Size = new System.Drawing.Size(240, 30);
+            this.buttonOrder.TabIndex = 8;
+            this.buttonOrder.Text = "Создать заказ";
+            this.buttonOrder.UseVisualStyleBackColor = true;
+            // 
+            // buttonKP
+            // 
+            this.buttonKP.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonKP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonKP.Location = new System.Drawing.Point(12, 361);
+            this.buttonKP.Name = "buttonKP";
+            this.buttonKP.Size = new System.Drawing.Size(240, 30);
+            this.buttonKP.TabIndex = 9;
+            this.buttonKP.Text = "Коммерческие предложения";
+            this.buttonKP.UseVisualStyleBackColor = true;
+            // 
+            // buttonContracts
+            // 
+            this.buttonContracts.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonContracts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonContracts.Location = new System.Drawing.Point(12, 397);
+            this.buttonContracts.Name = "buttonContracts";
+            this.buttonContracts.Size = new System.Drawing.Size(240, 30);
+            this.buttonContracts.TabIndex = 10;
+            this.buttonContracts.Text = "Договоры";
+            this.buttonContracts.UseVisualStyleBackColor = true;
+            // 
+            // buttonGraph
+            // 
+            this.buttonGraph.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonGraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGraph.Location = new System.Drawing.Point(12, 433);
+            this.buttonGraph.Name = "buttonGraph";
+            this.buttonGraph.Size = new System.Drawing.Size(240, 30);
+            this.buttonGraph.TabIndex = 11;
+            this.buttonGraph.Text = "Статус договоров";
+            this.buttonGraph.UseVisualStyleBackColor = true;
+            // 
+            // AccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 475);
+            this.Controls.Add(this.buttonGraph);
+            this.Controls.Add(this.buttonContracts);
+            this.Controls.Add(this.buttonKP);
+            this.Controls.Add(this.buttonOrder);
+            this.Controls.Add(this.buttonVendors);
+            this.Controls.Add(this.buttonProfile);
+            this.Controls.Add(this.labelMain);
+            this.Name = "AccountForm";
+            this.ShowIcon = false;
+            this.Text = "Личный кабинет";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMain;
+        private System.Windows.Forms.Button buttonProfile;
+        private System.Windows.Forms.Button buttonVendors;
+        private System.Windows.Forms.Button buttonOrder;
+        private System.Windows.Forms.Button buttonKP;
+        private System.Windows.Forms.Button buttonContracts;
+        private System.Windows.Forms.Button buttonGraph;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TradeAggregator
+{
+    public partial class MainForm : Form
+    {
+        private SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["AggregatorDataBase"].ConnectionString);
+        private bool _registrationFlag = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Загрузка формы
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _connection.Open();
+            if (_connection.State == ConnectionState.Closed || _connection.State == ConnectionState.Broken)
+                MessageBox.Show("Не удалось подключиться к базе данных.\nПроверьте подключение!","Ошибка подключения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            CheckState();
+            CheckSaveProfile();
+        }
+
+        // Изменения интерфейса для разных режимов
+        private void CheckState()
+        {
+            if(_registrationFlag) // Режим регистрации
+            {
+                labelMain.Text = "Регистрация";
+                Size = new Size(390, 320);
+                MinimumSize = new Size(390, 320);
+                MaximumSize = new Size(390, 320);
+                labelType.Visible = true;
+                labelPassRepeat.Visible = true;
+                comboBoxType.Visible = true;
+                textBoxPassRepeat.Visible = true;
+                checkBoxSave.Visible = false;
+                buttonSwitch.Text = "Отмена";
+                buttonApply.Text = "Регистрация";
+            }
+            else
+            {
+                labelMain.Text = "Авторизация";
+                Size = new Size(390, 260);
+                MinimumSize = new Size(390, 260);
+                MaximumSize = new Size(390, 260);
+                labelType.Visible = false;
+                labelPassRepeat.Visible = false;
+                comboBoxType.Visible = false;
+                textBoxPassRepeat.Visible = false;
+                checkBoxSave.Visible = true;
+                buttonSwitch.Text = "Регистрация";
+                buttonApply.Text = "Войти";
+            }
+        }
+
+        // Кнопка смены режимов
+        private void buttonSwitch_Click(object sender, EventArgs e)
+        {
+            if (_registrationFlag)
+                _registrationFlag = false;
+            else
+                _registrationFlag = true;
+            CheckState();
+        }
+
+        // Вход / регистрация
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            SqlCommand command;
+            SqlDataReader reader;
+            Int64 userRecID = 0;
+            bool saveFlag;
+
+            if (CheckNullTextBoxes())
+                return;
+
+            if(_registrationFlag) // Регистрация
+            {
+                if (textBoxPass.Text.Trim() != textBoxPassRepeat.Text.Trim())
+                {
+                    MessageBox.Show("Пароли не совпадают!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                command = new SqlCommand($"select RecID from Users where Login = " +
+                    $"'{textBoxLogin.Text.Trim()}'", _connection);
+                if(!(command.ExecuteScalar() is null))
+                {
+                    MessageBox.Show("Такой логин уже существует!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                command = new SqlCommand($"insert into Users values ({comboBoxType.SelectedIndex}, " +
+                    $"'{textBoxLogin.Text.Trim()}', '{textBoxPass.Text.Trim()}', null, null, null)", _connection);
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Регистрация успешно завершена.", "Поздравляем");
+            }
+            else // Авторизация
+            {
+                // Поиск логина
+                command = new SqlCommand($"select RecID from Users where Login = " +
+                    $"'{textBoxLogin.Text.Trim()}'", _connection);
+                if(command.ExecuteScalar() is null)
+                {
+                    MessageBox.Show("Логин не найден!\nПроверьте правильность введенных " +
+                        "данных или зарегистрируйтесь.", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Проверка пароля + получение ID и SaveFlag
+                command = new SqlCommand("select RecID, SaveFlag from Users where Login = " +
+                    $"'{textBoxLogin.Text.Trim()}' and Password = '{textBoxPass.Text.Trim()}'", _connection);
+                reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    userRecID = Convert.ToInt64(reader[0]);
+                    saveFlag = Convert.ToBoolean(reader[1]);
+                    reader.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Пароль введен не верно!\nПопробуйте снова.",
+                        "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    reader.Close();
+                    return;
+                }
+
+                // Вкл или выкл режима запоминания
+                if(saveFlag != checkBoxSave.Checked)
+                {
+                    if(checkBoxSave.Checked) // Вкл
+                    {
+                        command = new SqlCommand($"update Users set SaveFlag = 1, PCName = '{Environment.UserDomainName}', " +
+                            $"UserName = '{Environment.UserName}'", _connection);
+                        command.ExecuteNonQuery();
+                    }
+                    else // Выкл
+                    {
+                        command = new SqlCommand($"update Users set SaveFlag = 0", _connection);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Вы авторизовались", "Поздравляем");
+                // Открытие формы личного кабинета
+                //
+                // ..........
+                // ..........
+                //
+            }
+        }
+
+
+        // Проверка полей на заполненность
+        private bool CheckNullTextBoxes()
+        {
+
+            return false;
+        }
+
+        // Поиск сохраненных данных для входа на этом устройстве
+        private void CheckSaveProfile()
+        {
+            bool saveFlag;
+
+            SqlCommand command = new SqlCommand($"select Login, Password, SaveFlag from Users " +
+                $"where PCName = '{Environment.UserDomainName}' and " +
+                $"UserName = '{Environment.UserName}'", _connection);
+            SqlDataReader reader = command.ExecuteReader();
+            if(reader.Read())
+            {
+                saveFlag = Convert.ToBoolean(reader[2]);
+                if(saveFlag)
+                {
+                    textBoxLogin.Text = reader[0].ToString();
+                    textBoxPass.Text = reader[1].ToString();
+                    checkBoxSave.Checked = saveFlag;
+                }
+            }
+            reader.Close();
+        }
+
+        // Закрытие формы
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _connection.Close();
+        }
+
+        
+    }
+}
